89b1a699bd6e90055f2ea118f9ebeeef6a68e467
Vinicius Souza <visouza@microsoft.com>
2020-07-24 15:38:19 -0700

    TEST: Add tests for DeleteTableForUser

--- src/Arriba/Arriba.Test/Services/ArribaManagementServiceTests.cs
+++ src/Arriba/Arriba.Test/Services/ArribaManagementServiceTests.cs
@@ -383,5 +383,49 @@ namespace Arriba.Test.Services
             _service.ReloadTableForUser(tableName, _writer);
             _service.ReloadTableForUser(tableName, _owner);
         }
+
+
+        [DataTestMethod]
+        [DataRow("foo")]
+        public void DeleteTableForUserTableNotFound(string tableName)
+        {
+            Assert.ThrowsException<TableNotFoundException>(() => _service.DeleteTableForUser(tableName, _owner));
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("  ")]
+        [DataRow("")]
+        public void DeleteTableForUserTableNameMissing(string tableName)
+        {
+            Assert.ThrowsException<ArgumentException>(() => _service.DeleteTableForUser(tableName, _owner));
+        }
+
+        [DataTestMethod]
+        [DataRow(TableName)]
+        public void DeleteTableForUserUnauthorizedUser(string tableName)
+        {
+            Assert.ThrowsException<ArribaAccessForbiddenException>(() => _service.DeleteTableForUser(tableName, _nonAuthenticatedUser));
+        }
+
+        [DataTestMethod]
+        [DataRow(TableName)]
+        public void DeleteTableForUserOwner(string tableName)
+        {
+            DeleteTableForUser(tableName, _owner);
+        }
+
+        [DataTestMethod]
+        [DataRow(TableName)]
+        public void DeleteTableForUserWriter(string tableName)
+        {
+            DeleteTableForUser(tableName, _owner);
+        }
+
+        private void DeleteTableForUser(string tableName, IPrincipal user)
+        {
+            _service.DeleteTableForUser(tableName, user);
+        }
+
     }
 }
