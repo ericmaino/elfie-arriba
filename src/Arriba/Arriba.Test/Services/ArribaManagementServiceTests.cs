a5b02b5be05cc24c118b8c92747cd00b2bac68b7
Vinicius Souza <visouza@microsoft.com>
2020-07-24 14:26:57 -0700

    TEST: Adding tests for SaveTableForUser

--- src/Arriba/Arriba.Test/Services/ArribaManagementServiceTests.cs
+++ src/Arriba/Arriba.Test/Services/ArribaManagementServiceTests.cs
@@ -305,5 +305,51 @@ namespace Arriba.Test.Services
             AddColumnsToTableForUser(tableName, _writer);
         }
 
+        [DataTestMethod]
+        [DataRow("foo")]
+        public void SaveTableForUserTableNotFound(string tableName)
+        {
+            Assert.ThrowsException<TableNotFoundException>(() => _service.SaveTableForUser(tableName, _owner, VerificationLevel.Normal));
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("  ")]
+        [DataRow("")]
+        public void SaveTableForUserTableNameMissing(string tableName)
+        {
+            Assert.ThrowsException<ArgumentException>(() => _service.SaveTableForUser(tableName, _owner, VerificationLevel.Normal));
+        }
+
+        [DataTestMethod]
+        [DataRow(TableName)]
+        public void SaveTableForUserUnauthorizedUser(string tableName)
+        {
+            Assert.ThrowsException<ArribaAccessForbiddenException>(() => _service.SaveTableForUser(tableName, _nonAuthenticatedUser, VerificationLevel.Normal));
+            Assert.ThrowsException<ArribaAccessForbiddenException>(() => _service.SaveTableForUser(tableName, _reader, VerificationLevel.Normal));
+        }
+
+        [DataTestMethod]
+        [DataRow(TableName)]
+        public void SaveTableForUserInconsistent(string tableName)
+        {
+            CheckTableColumnsQuantity(tableName, 2);
+            _service.AddColumnsToTableForUser(tableName, GetColumnDetailsList(), _owner);
+
+            var result = _service.SaveTableForUser(tableName, _owner, VerificationLevel.Normal);
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Item1);
+            Assert.IsFalse(result.Item2.Succeeded);
+        }
+
+        [DataTestMethod]
+        [DataRow(TableName)]
+        public void SaveTableForUser(string tableName)
+        {
+            var result = _service.SaveTableForUser(tableName, _owner, VerificationLevel.Normal);
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Item1);
+            Assert.IsTrue(result.Item2.Succeeded);
+        }
     }
 }
