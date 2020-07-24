202656897d956422f37b900acdbd18c0a9cb675e
Vinicius Souza <visouza@microsoft.com>
2020-07-24 15:13:30 -0700

    TEST: Add test for ReloadTableForUser

--- src/Arriba/Arriba.Test/Services/ArribaManagementServiceTests.cs
+++ src/Arriba/Arriba.Test/Services/ArribaManagementServiceTests.cs
@@ -351,5 +351,37 @@ namespace Arriba.Test.Services
             Assert.IsTrue(result.Item1);
             Assert.IsTrue(result.Item2.Succeeded);
         }
+
+        [DataTestMethod]
+        [DataRow("foo")]
+        public void ReloadTableForUserTableNotFound(string tableName)
+        {
+            Assert.ThrowsException<TableNotFoundException>(() => _service.ReloadTableForUser(tableName, _owner));
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("  ")]
+        [DataRow("")]
+        public void ReloadTableForUserTableNameMissing(string tableName)
+        {
+            Assert.ThrowsException<ArgumentException>(() => _service.ReloadTableForUser(tableName, _owner));
+        }
+
+        [DataTestMethod]
+        [DataRow(TableName)]
+        public void ReloadTableForUserUnauthorizedUser(string tableName)
+        {
+            Assert.ThrowsException<ArribaAccessForbiddenException>(() => _service.ReloadTableForUser(tableName, _nonAuthenticatedUser));            
+        }
+                
+        [DataTestMethod]
+        [DataRow(TableName)]
+        public void ReloadTableForUser(string tableName)
+        {
+            _service.ReloadTableForUser(tableName, _reader);
+            _service.ReloadTableForUser(tableName, _writer);
+            _service.ReloadTableForUser(tableName, _owner);
+        }
     }
 }
