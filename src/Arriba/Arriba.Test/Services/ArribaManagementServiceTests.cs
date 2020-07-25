9bff2e74faf0e0dbdd466983f668d9354da4fe3b
Vinicius Souza <visouza@microsoft.com>
2020-07-24 19:04:57 -0700

    TEST: Add test for DeleteTableRowsForUser

--- src/Arriba/Arriba.Test/Services/ArribaManagementServiceTests.cs
+++ src/Arriba/Arriba.Test/Services/ArribaManagementServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Arriba.Communication.Server.Application;
 using Arriba.Model;
 using Arriba.Model.Column;
+using Arriba.Model.Query;
 using Arriba.Model.Security;
 using Arriba.Server.Hosting;
 using Arriba.Structures;
@@ -425,6 +426,59 @@ namespace Arriba.Test.Services
         private void DeleteTableForUser(string tableName, IPrincipal user)
         {
             _service.DeleteTableForUser(tableName, user);
+        }
+
+
+        [DataTestMethod]
+        [DataRow("foo")]
+        public void DeleteTableRowsForUserTableNotFound(string tableName)
+        {
+            Assert.ThrowsException<TableNotFoundException>(() => _service.DeleteTableRowsForUser(tableName, "ID = 1", _owner));
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("  ")]
+        [DataRow("")]
+        public void DeleteTableRowsForUserTableNameMissing(string tableName)
+        {
+            Assert.ThrowsException<ArgumentException>(() => _service.DeleteTableRowsForUser(tableName, "ID = 1", _owner));
+        }
+
+        [DataTestMethod]
+        [DataRow(TableName)]
+        public void DeleteTableRowsForUserUnauthorizedUser(string tableName)
+        {
+            Assert.ThrowsException<ArribaAccessForbiddenException>(() => _service.DeleteTableRowsForUser(tableName, "ID = 1", _nonAuthenticatedUser));
+        }
+
+        [DataTestMethod]
+        [DataRow(TableName, "ID = 1 OR Name = Vinicius")]
+        [DataRow(TableName, "ID = 2")]
+        [DataRow(TableName, "ID = 99")]
+        public void DeleteTableRowsForUserOwner(string tableName, string query)
+        {
+            DeleteTableRowsForUser(tableName, query, _owner);
+        }
+
+        [DataTestMethod]
+        [DataRow(TableName, "ID = 1 OR Name = Vinicius")]
+        [DataRow(TableName, "ID = 2")]
+        [DataRow(TableName, "ID = 99")]
+        public void DeleteTableRowsForUserWriter(string tableName, string query)
+        {
+            DeleteTableRowsForUser(tableName, query, _owner);
+        }
+
+        private void DeleteTableRowsForUser(string tableName, string query, IPrincipal user)
+        {
+            var table = _db[tableName];
+            var countBefore = table.Count;
+            var result = _service.DeleteTableRowsForUser(tableName, query, user);
+            if (result.Count > 0)
+                Assert.AreEqual(countBefore - result.Count, table.Count);
+            else
+                Assert.AreEqual(countBefore, table.Count);
         }
 
     }
