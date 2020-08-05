63b3e25298508efec3b051a038fabe8b8293d92d
Vinicius Souza <visouza@microsoft.com>
2020-08-04 19:15:17 -0700

    HACK: Add volatile database security to check against claims

--- src/Arriba/Arriba.Communication/Server/Application/ArribaManagementServiceFactory.cs
+++ src/Arriba/Arriba.Communication/Server/Application/ArribaManagementServiceFactory.cs
@@ -1,7 +1,10 @@
 ﻿using Arriba.Communication.Model;
 using Arriba.Model;
 using Arriba.Model.Correctors;
+using Arriba.Model.Security;
 using Arriba.ParametersCheckers;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Arriba.Communication.Server.Application
 {
@@ -24,6 +27,17 @@ namespace Arriba.Communication.Server.Application
                 userAliasCorrectorTable = Table_People;
 
             var correctors = new CompositionComposedCorrectors(new TodayCorrector(), new UserAliasCorrector(secureDatabase[userAliasCorrectorTable]));
+
+            //HACK: As the crawler don't have the AppRoles available at the JWT Token as claims
+            //needed to grant for each role (PermissionScope) the roles name must use the same name as the Scope.
+            secureDatabase.TableNames.ToList().ForEach(table =>
+            {
+                var security = secureDatabase.Security(table);
+                security.Grant(IdentityScope.Group, PermissionScope.Reader.ToString(), PermissionScope.Reader);
+                security.Grant(IdentityScope.Group, PermissionScope.Writer.ToString(), PermissionScope.Writer);
+                security.Grant(IdentityScope.Group, PermissionScope.Owner.ToString(), PermissionScope.Owner);                
+            });
+            
 
             return new ArribaManagementService(secureDatabase, correctors);
         }
