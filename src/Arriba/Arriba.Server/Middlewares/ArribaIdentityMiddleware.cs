ebbefe5d5cbb8cd8a1fa17686ff0c68d7e93b013
Vinicius Souza <visouza@microsoft.com>
2020-08-04 17:00:21 -0700

    FEATURE: Add Middleware ArribaIdentityMiddleware

---
+++ src/Arriba/Arriba.Server/Middlewares/ArribaIdentityMiddleware.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Security.Claims;
+using Arriba.Server.Models;
+
+namespace Arriba.Server.Middlewares
+{
+    public class ArribaIdentityMiddleware
+    {
+        private const string AuthorizationKey = "authorization";
+        private const string BearerTokenPrefix = "Bearer";
+        private const string AuthenticationType = "JWTTokenGenerated";
+        private readonly RequestDelegate _next;
+
+        public ArribaIdentityMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, IArribaServerConfiguration serverConfiguration)
+        {
+            if (!serverConfiguration.EnabledAuthentication)
+                await _next(httpContext);
+
+            var authHeader = httpContext.Request.Headers[AuthorizationKey];
+            var bearerToken = authHeader.Where(s => s.StartsWith(BearerTokenPrefix)).FirstOrDefault();
+
+            if (string.IsNullOrWhiteSpace(bearerToken))
+                await _next(httpContext);
+
+            bearerToken = bearerToken.Substring($"{BearerTokenPrefix} ".Length);
+
+            var user = CreatePrincipal(bearerToken);
+            httpContext.User = user;
+
+            await _next(httpContext);
+        }
+
+        private ClaimsPrincipal CreatePrincipal(string accessToken)
+        {
+            var parts = accessToken.Split('.');
+
+            if (parts.Length != 3)
+                return null;
+
+            var payload = parts[1];            
+
+            var token = TryToGetTokenObject(payload);
+            ClaimsPrincipal user = GetPrincipalFromToken(token);
+
+            return user;
+        }
+
+        private ClaimsPrincipal GetPrincipalFromToken(Token token)
+        {
+            if (token == null)
+                return null;
+
+            var identity = new ClaimsIdentity(new GenericIdentity(token.Name, AuthenticationType));
+            token.Roles.ToList().ForEach(role =>
+            {
+                var claim = new Claim(identity.RoleClaimType, role.ToLower());
+                identity.AddClaim(claim);
+            });
+
+            var user = new ClaimsPrincipal(identity);
+            return user;
+        }
+
+        private Token TryToGetTokenObject(string payload)
+        {
+            if (string.IsNullOrWhiteSpace(payload))
+                return null;
+
+            var decodedPayload = Base64UrlEncoder.Decode(payload);
+            Trace.WriteLine($"decodedPayload: {decodedPayload}");
+
+            return Token.FromJson(decodedPayload);
+        }
+    }
+}
---
+++ src/Arriba/Arriba.Server/Models/Token.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Arriba.Server.Models
+{
+
+    public partial class Token
+    {
+        [JsonProperty("aud")]
+        public Guid Aud { get; set; }
+
+        [JsonProperty("iss")]
+        public Uri Iss { get; set; }
+
+        [JsonProperty("iat")]
+        public long Iat { get; set; }
+
+        [JsonProperty("nbf")]
+        public long Nbf { get; set; }
+
+        [JsonProperty("exp")]
+        public long Exp { get; set; }
+
+        [JsonProperty("aio")]
+        public string Aio { get; set; }
+
+        [JsonProperty("azp")]
+        public Guid Azp { get; set; }
+
+        [JsonProperty("azpacr")]
+        public string Azpacr { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("oid")]
+        public Guid Oid { get; set; }
+
+        [JsonProperty("preferred_username")]
+        public string PreferredUsername { get; set; }
+
+        [JsonProperty("rh")]
+        public string Rh { get; set; }
+
+        [JsonProperty("roles")]
+        public string[] Roles { get; set; }
+
+        [JsonProperty("scp")]
+        public string Scp { get; set; }
+
+        [JsonProperty("sub")]
+        public string Sub { get; set; }
+
+        [JsonProperty("tid")]
+        public Guid Tid { get; set; }
+
+        [JsonProperty("uti")]
+        public string Uti { get; set; }
+
+        [JsonProperty("ver")]
+        public string Ver { get; set; }
+    }
+
+    public partial class Token
+    {
+        public static Token FromJson(string json) => JsonConvert.DeserializeObject<Token>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Token self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+
+}
+
