b07ed7f6b79d5fc6e7f33878d5df6db1925f9a1e
Vinicius Souza <visouza@microsoft.com>
2020-08-04 17:06:18 -0700

    FEATURE: Add extension ArribaAuthorizationExtension

---
+++ src/Arriba/Arriba.Server/Extensions/ArribaAuthorizationExtension.cs
@@ -0,0 +1,15 @@
+﻿using Arriba.Server.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace Arriba.Server.Extensions
+{
+    public static class ArribaAuthorizationExtension
+    {
+        public static IApplicationBuilder UseGetPrincipalFromToken(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ArribaIdentityMiddleware>();
+        }
+
+    }
+}
