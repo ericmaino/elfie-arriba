e407bd726133db97ef7c55f52f804e99e240e5bb
Eric Maino <ericmai@microsoft.com>
2020-08-24 14:23:24 -0700

    FEATURE: Begin moving to unified execution model
    - This introuces a common startup path using ArribaProgram. The long term goal is that the Main method are only a few lines long and everything else has been moved into composable units.

---
+++ src/Arriba/Arriba/ArribaProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Arriba.Diagnostics.Tracing;
+
+namespace Arriba
+{
+    public class ArribaProgram
+    {
+        public static async Task Run<T>(Func<Task> program)
+        {
+            await program();
+        }
+
+        public static async Task<int> Run<T>(Func<Task<int>> program)
+        {
+            var x = new ReturnCode();
+
+            await Run<T>(async () =>
+            {
+                x.Value = await program();
+            });
+
+            return x.Value;
+        }
+
+        private class ReturnCode
+        {
+            public int Value { get; set; }
+        }
+    }
+}
