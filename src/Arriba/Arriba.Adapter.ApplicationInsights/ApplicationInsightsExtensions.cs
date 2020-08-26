35073f9542c35c0781e2e48e36f67a094060ebee
Eric Maino <eric.maino@microsoft.com>
2020-08-26 13:42:57 -0700

    WIP: Adding basic No-Op EventFlow support

--- src/Arriba/Arriba.Adapter.ApplicationInsights/ApplicationInsightsExtensions.cs
+++ src/Arriba/Arriba.Adapter.ApplicationInsights/ApplicationInsightsExtensions.cs
@@ -1,8 +1,17 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using Arriba.Telemetry;
 using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.AspNetCore;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.ApplicationInsights.TraceListener;
+using Microsoft.Diagnostics.EventFlow;
+using Microsoft.Diagnostics.EventFlow.ApplicationInsights;
+using Microsoft.Diagnostics.EventFlow.HealthReporters;
+using Microsoft.Diagnostics.EventFlow.Inputs;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Arriba
@@ -22,6 +31,7 @@ namespace Arriba
                 });
 
                 services.AddSingleton<ITelemetryInitializer>(new ReportServiceNameProcessor(config.AppConfig.ServiceName));
+                services.UseEventFlow();
             }
         }
 
@@ -30,6 +40,48 @@ namespace Arriba
             Trace.WriteLine("Registering Application Insights trace listener");
             var listener = new ApplicationInsightsTraceListener(config.InstrumentationKey);
             Trace.Listeners.Add(listener);
+        }
+
+
+        private static void UseEventFlow(this IServiceCollection services)
+        {
+            var healthReport = new NoOpHealthReporter();
+            var aiInput = new ApplicationInsightsInputFactory().CreateItem(null, healthReport);
+            var inputs = new IObservable<EventData>[] { aiInput };
+            var sinks = new EventSink[]
+            {
+                new EventSink(new NoOpOutput(), null)
+            };
+
+            var p = new DiagnosticPipeline(healthReport, inputs, null, sinks, null, disposeDependencies: true);
+            services.AddSingleton<ITelemetryProcessorFactory>(new EventFlowTelemetryProcessorFactory(p));
+        }
+
+        private class NoOpHealthReporter : IHealthReporter
+        {
+            public void Dispose()
+            {
+            }
+
+            public void ReportHealthy(string description = null, string context = null)
+            {
+            }
+
+            public void ReportProblem(string description, string context = null)
+            {
+            }
+
+            public void ReportWarning(string description, string context = null)
+            {
+            }
+        }
+
+        private class NoOpOutput : IOutput
+        {
+            public Task SendEventsAsync(IReadOnlyCollection<EventData> events, long transmissionSequenceNumber, CancellationToken cancellationToken)
+            {
+                return Task.CompletedTask;
+            }
         }
     }
 }
