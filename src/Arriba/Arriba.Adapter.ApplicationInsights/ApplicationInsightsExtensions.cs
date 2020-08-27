3378df11b5633c6c2f04759c33b4de59cdfc0540
Eric Maino <eric.maino@microsoft.com>
2020-08-26 22:35:42 -0700

    WIP: Testing what splunk looks like

--- src/Arriba/Arriba.Adapter.ApplicationInsights/ApplicationInsightsExtensions.cs
+++ src/Arriba/Arriba.Adapter.ApplicationInsights/ApplicationInsightsExtensions.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Arriba.Telemetry;
-using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.AspNetCore;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.ApplicationInsights.TraceListener;
 using Microsoft.Diagnostics.EventFlow;
 using Microsoft.Diagnostics.EventFlow.ApplicationInsights;
-using Microsoft.Diagnostics.EventFlow.HealthReporters;
 using Microsoft.Diagnostics.EventFlow.Inputs;
 using Microsoft.Extensions.DependencyInjection;
+using Spotsoft.Diagnostics.EventFlow.Outputs.Splunk;
+using Spotsoft.Diagnostics.EventFlow.Outputs.Splunk.Configuration;
 
 namespace Arriba
 {
@@ -45,16 +47,45 @@ namespace Arriba
 
         private static void UseEventFlow(this IServiceCollection services)
         {
-            var healthReport = new NoOpHealthReporter();
-            var aiInput = new ApplicationInsightsInputFactory().CreateItem(null, healthReport);
-            var inputs = new IObservable<EventData>[] { aiInput };
-            var sinks = new EventSink[]
+            services.AddTransient<IHealthReporter, NoOpHealthReporter>();
+            services.AddTransient<IOutput, NoOpOutput>();
+            
+            var config = new SplunkOutputConfiguration()
             {
-                new EventSink(new NoOpOutput(), null)
+                Host = "localhost",
+                ServiceBaseAddress = "http://localhost:8088",
+                AuthenticationToken = "TOKEN HERE"
             };
 
-            var p = new DiagnosticPipeline(healthReport, inputs, null, sinks, null, disposeDependencies: true);
-            services.AddSingleton<ITelemetryProcessorFactory>(new EventFlowTelemetryProcessorFactory(p));
+            services.AddTransient<IOutput>( p=> new SplunkOutputFactory().CreateItem(config, p.GetService<IHealthReporter>()));
+            services.AddSingleton<IPipelineFactory, PipelineFactory>();
+            services.AddSingleton<DiagnosticPipeline>(p => p.GetService<IPipelineFactory>().CreatePipeline());
+            services.AddSingleton<ITelemetryProcessorFactory, EventFlowTelemetryProcessorFactory>();
+        }
+
+        public interface IPipelineFactory
+        {
+            DiagnosticPipeline CreatePipeline();
+        }
+
+        private class PipelineFactory : IPipelineFactory
+        {
+            private readonly IHealthReporter _reporter;
+            private readonly IList<IOutput> _outputs;
+
+            public PipelineFactory(IHealthReporter reporter, IEnumerable<IOutput> outputs)
+            {
+                _reporter = reporter;
+                _outputs = outputs.ToImmutableList();
+            }
+
+            public DiagnosticPipeline CreatePipeline()
+            {
+                var aiInput = new ApplicationInsightsInputFactory().CreateItem(null, _reporter);
+                var inputs = new IObservable<EventData>[] { aiInput };
+                var sinks = _outputs.Select(x => new EventSink(x, null)).ToArray();
+                return new DiagnosticPipeline(_reporter, inputs, null, sinks, null, disposeDependencies: true);
+            }
         }
 
         private class NoOpHealthReporter : IHealthReporter
