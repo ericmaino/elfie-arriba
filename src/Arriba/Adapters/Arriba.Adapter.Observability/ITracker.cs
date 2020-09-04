1b02689979ae14a2ed2561ebee20c414376d0967
Vinicius Souza <visouza@microsoft.com>
2020-09-04 11:10:20 -0700

    Implementing Proxy dispatcher

---
+++ src/Arriba/Adapters/Arriba.Adapter.Observability/ITracker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace Arriba.Adapter.Observability
+{
+    public interface ITracker
+    {
+        void LogAfter(MethodInfo methodInfo, object[] args, object result);
+
+        void LogBefore(MethodInfo methodInfo, object[] args);
+
+        void LogException(Exception exception, MethodInfo methodInfo = null);
+
+    }
+}
---
+++ src/Arriba/Adapters/Arriba.Adapter.Observability/Proxy.cs
@@ -0,0 +1,44 @@
+﻿using Arriba.Serialization;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Arriba.Adapter.Observability
+{
+    public class Proxy<T> : DispatchProxy
+    {
+        private T _decorated;
+        private ITracker _tracker;
+
+        public static T Create(T decorated, ITracker tracker)
+        {
+            object proxy = Create<T, Proxy<T>>();
+            ((Proxy<T>)proxy).SetParameters(decorated, tracker);
+
+            return (T)proxy;
+        }
+
+        private void SetParameters(T decorated, ITracker tracker)
+        {
+            _decorated = decorated;
+            _tracker = tracker;
+        }
+
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
+        {
+            if (targetMethod == null) throw new ArgumentNullException(nameof(targetMethod));
+
+            _tracker.LogBefore(targetMethod, args);
+            try
+            {
+                var result = targetMethod.Invoke(_decorated, args);
+            }
+            catch (Exception ex)
+            {
+                _tracker.LogException(ex, targetMethod);
+            }
+
+            return null;
+        }
+    }
+}
